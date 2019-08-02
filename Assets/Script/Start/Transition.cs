3dc33fa1175c2ff1b458bd566b4db8adc66650e2
Minoricawa <cruel-fancy.lie@live.jp>
2019-08-02 18:33:18 +0900

    スタート画面に終了ボタン追加

---
+++ Assets/Script/Start/Transition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    // タイトル画面へ
+    public void NextTitle()
+    {
+        SceneManager.LoadScene("Scene_title");
+    }
+
+    // セレクト画面へ
+    public void NextSelect()
+    {
+        SceneManager.LoadScene("Scene_kyokai");
+    }
+
+    // ゲーム終了
+    public void GameExit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
