28ed71779ab025beea7f5948802efece80a47bf5
Minoricawa <cruel-fancy.lie@live.jp>
2019-08-30 20:35:10 +0900

    コントローラーからのビーム実装

---
+++ Assets/Script/vr/Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] LineRenderer beam;
+    public GameObject reticle = null;
+
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Input_Sources HandType;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //beam.SetActive(false);
+        beam.startWidth = 0.001f;
+        beam.endWidth = 0.001f;
+        beam.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            beam.SetPosition(0, this.transform.position);
+            beam.SetPosition(1, hit.point);
+
+            if (reticle == null)
+            {
+                reticle.transform.position = hit.point;
+            }
+
+            if (SteamVR_Actions.default_Teleport.GetStateDown(HandType))
+            {
+                Debug.Log(hit.collider.name);
+
+                if (hit.collider.name == "aaa")
+                {
+                    Debug.Log("aaaにクリック");
+
+                }
+            }
+            
+
+        }
+
+
+    }
+}
