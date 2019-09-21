66fadbff54021a8194916835c811b0778b4bc0da
Minoricawa <cruel-fancy.lie@live.jp>
2019-09-21 17:42:12 +0900

    加速度によりSEが鳴る仕様追加、ノーツとの衝突判定変更

--- Assets/Script/vr/LaserController.cs
+++ Assets/Script/vr/LaserController.cs
@@ -5,22 +5,29 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
+using UnityEngine.XR;
+
 
 public class LaserController : MonoBehaviour
 {
-    // 以下メンバ変数定義.
-    protected GameObject laser;
-    protected GameObject cursor;
-
+    // 以下公開メンバ変数定義.
     public float thickness = 0.002f;
     public float cursorSize = 0.04f;
     public Color laserColor = new Color(1, 1, 0);
-    public SteamVR_Input_Sources HandType;
+ //   public SteamVR_Input_Sources HandType;
     public System.Action hit_callback;
-
+   
+    // 以下メンバ変数定義.
+    protected GameObject laser;
+    protected GameObject cursor;
     GameObject hit_note = null;
+    VelocityEstimator ve = null;
+    AudioSource audio_source = null;
+    [SerializeField] AudioClip se = null;
+    float speed = 0;
 
-
+    
     // 以下プロパティ.
     public System.Action HitCallback
     {
@@ -35,6 +42,8 @@ public class LaserController : MonoBehaviour
 
     void Start()
     {
+        ve = GetComponent<VelocityEstimator>();
+        audio_source = GetComponent<AudioSource>();
 
         //レーザーポインタを作成する
         CreateLaserPointer();
@@ -51,12 +60,14 @@ public class LaserController : MonoBehaviour
         laser.GetComponent<MeshRenderer>().material.color = laserColor;
         Object.DestroyImmediate(laser.GetComponent<BoxCollider>());
 
+        /*
         cursor = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         cursor.transform.SetParent(transform, false);
         cursor.transform.localScale = new Vector3(cursorSize, cursorSize, cursorSize);
         cursor.transform.localPosition = new Vector3(0.0f, 0.0f, 2.0f);
         cursor.GetComponent<MeshRenderer>().material.color = laserColor;
         Object.DestroyImmediate(cursor.GetComponent<SphereCollider>());
+        */
     }
 
     public void AdjustLaserDistance(float distance)
@@ -68,21 +79,55 @@ public class LaserController : MonoBehaviour
         //レーザーの長さを調整
         laser.transform.localScale = new Vector3(thickness, thickness, distance);
         laser.transform.localPosition = new Vector3(0.0f, 0.0f, distance * 0.5f);
-        cursor.transform.localPosition = new Vector3(0.0f, 0.0f, distance);
+     //   cursor.transform.localPosition = new Vector3(0.0f, 0.0f, distance);
     }
+
+    
 
     void Update()
     {
         //LaserInputModule.test = SteamVR_Actions.default_Teleport.GetStateDown(HandType);
+
+        /*
+        if (speed >= 10.0f)
+        {
+            Swing();
+        }
+        */
+
+        //speed = ve.GetVelocityEstimate().magnitude;
+        speed = ve.GetVelocityEstimate().magnitude;
+        if (ve.GetAccelerationEstimate().magnitude > 400)
+        {
+            Swing();
+        }
+         //Debug.LogFormat("speed {0}", speed);
+         //Debug.LogFormat("GetAccelerationEstimate {0}", ve.GetAccelerationEstimate().magnitude);
     }
 
+    // ノーツに触れたときHit
     void OnTriggerEnter(Collider other)
     {
-        if (hit_note == other) return;
+        if (ve.GetAccelerationEstimate().magnitude < 300) return;
+        if (hit_note == other.gameObject) return;
         if (other.tag != "enemy") return;
         hit_note = other.gameObject;
         if (hit_callback != null) hit_callback();
-        
+    }
+
+    // 音を鳴らす
+    void Swing()
+    {
+        Debug.LogFormat("Swing1");
+       if (audio_source.isPlaying) return;
+        Debug.LogFormat("Swing2");
+        audio_source.PlayOneShot(se);
+
+        //sXRSettings.enabled = false;
+        Camera cam = GameObject.Find("Camera").GetComponent<Camera>();
+        XRDevice.DisableAutoXRCameraTracking(cam, false);
+        cam.gameObject.transform.localPosition = Vector3.zero;
+        cam.gameObject.transform.localRotation = Quaternion.identity;
 
     }
 }
